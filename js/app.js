e6bc07e5c244752d4d6c4b917f442c1fb0752bc6
Sergio Segura Blanco <ssegurab@gmail.com>
2022-03-19 02:10:53 +0100

    Aplicando ajustes finales

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@
 
 const enlaces = document.querySelectorAll(".ul .a");
 const lightbox = document.querySelector(".lightbox");
-const grande = document.querySelector('.grande')
-const cerrar = document.querySelector('.cerrar')
+const grande = document.querySelector('.grande');
+const cerrar = document.querySelector('.cerrar');
 
 enlaces.forEach((cadaEnlace, i) => {
     enlaces[i].addEventListener("click", (e) => {
@@ -30,4 +30,4 @@ enlaces.forEach((cadaEnlace, i) => {
 cerrar.addEventListener('click', () => {
     lightbox.classList.remove('activo')
     /* grande.setAttribute('src', '') */
-})
+});
